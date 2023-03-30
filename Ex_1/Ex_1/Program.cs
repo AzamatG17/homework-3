30ac8d2a7944588e195e9b95998b7751c5756e14
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2023-03-30 21:25:55 +0500

    commit

---
+++ Ex_1/Ex_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write num 10 : ");
+            int num = int.Parse(Console.ReadLine());
+            int num2, i = 1;
+            string to = "";
+            while (num != 0)
+            {
+                to += num % 2;
+                num2 = num /= 2;
+                num = num2;
+
+            }
+            Console.WriteLine(to);
+            string wo = "";
+            for (int j = to.Length - 1; j >= 0; j--)
+            {
+                Console.Write(to[j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
