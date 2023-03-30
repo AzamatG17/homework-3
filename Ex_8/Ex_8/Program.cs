15e4643d6288891ec4e4dc6aa303b65f519cdd00
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2023-03-30 22:31:22 +0500

    commit8

---
+++ Ex_8/Ex_8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+                }
+                if (sum == i)
+                {
+                    Console.Write($" {i},");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
