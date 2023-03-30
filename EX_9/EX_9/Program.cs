82ecd43dcdc0d453e0e2b9ddd721196499865c41
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2023-03-30 22:33:34 +0500

    commit9

---
+++ EX_9/EX_9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" : ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 != 0 && num % 3 != 0)
+            {
+                Console.WriteLine($"{num} is a prime num");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is not a prime num");
+            }
+            Main(args);
+        }
+    }
+}
