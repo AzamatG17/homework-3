031e1de6b93764e5ef9ccfda0577112afa788e8d
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2023-03-30 21:58:00 +0500

    commit4

---
+++ Ex_4/Ex_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % num == 0)
+                {
+                    Console.Write($" {i},");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
