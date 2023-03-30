ba3e2107d38b19963b7f994bad3867d4e913b16f
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2023-03-30 22:56:24 +0500

    commit10

---
+++ Ex_10/Ex_10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int fib = 1,sum = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (i >= 1)
+                {
+                    fib = sum + fib;
+                    sum = fib - sum;
+                    Console.Write($" {sum},");
+                }
+                else
+                {
+                    Console.Write($" {sum},");
+                }
+            }
+        }
+    }
+}
