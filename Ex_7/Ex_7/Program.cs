a3435a040b281628cf1295fd88daf927f2f7146e
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2023-03-30 22:14:57 +0500

    comiit7

---
+++ Ex_7/Ex_7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int fak = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                fak *= i;
+            }
+            Console.WriteLine($" Faktorial : {fak}");
+        }
+    }
+}
